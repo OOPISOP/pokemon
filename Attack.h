0eb81c9afb736d8455074a968e6234db17d5320f
Toby1009 <69885352+Toby1009@users.noreply.github.com>
2023-06-13 10:29:51 +0800

    save

--- Attack.h
+++ Attack.h
@@ -15,7 +15,7 @@
 #define TYPESIZE 18;
 using namespace std;
 
-//normal ~ fairy
+//normal ~ fairy type effect
 const double TYPE_EFFECT[18][18] = {{  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,0.5,  0,  1,  1,0.5,  1},//normal
                                     {  1,0.5,0.5,  1,  2,  2,  1,  1,  1,  1,  1,  2,0.5,  1,0.5,  1,  2,  1},//fire
                                     {  1,  2,0.5,  1,0.5,  1,  1,  1,  2,  1,  1,  1,  2,  1,0.5,  1,  1,  1},//water
@@ -35,9 +35,15 @@ const double TYPE_EFFECT[18][18] = {{  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1
                                     {  1,0.5,0.5,0.5,  1,  2,  1,  1,  1,  1,  1,  1,  2,  1,  1,  1,0.5,  2},//steel
                                     {  1,0.5,  1,  1,  1,  1,  2,0.5,  1,  1,  1,  1,  1,  1,  2,  2,0.5,  1}//fairy
                                    };
-
+/**
+ * LevelDamage = (2*Level+10)/250 = (2*50+10)/250 = 0.44
+ * Damage Formula, Damage = (LevelDamage*Power*A/D+2)*Critical*STAB*Type
+ * 𝐿𝑒𝑣𝑒𝑙 is the level of the attacking Pokémon. Default is 50.
+ * 𝑃𝑜𝑤𝑒𝑟 is the effective power of the used move.
+ * 𝐶𝑟𝑖𝑡𝑖𝑐𝑎𝑙 is 1.5 for a critical hit, otherwise 1.
+ * 𝑆𝑇𝐴𝐵 is the same-type attack bonus. This is equal to 1.5 if the move's type matches any of the user's types.
+**/
 //Implement damage logic
-
 class Attack
 {
 private:
